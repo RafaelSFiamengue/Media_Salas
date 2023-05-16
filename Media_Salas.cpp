1ae2a68afa66d9c0bdbdbbcfb53d911b898065fd
RafaelSFiamengue <127798981+RafaelSFiamengue@users.noreply.github.com>
2023-05-15 21:43:47 -0300

    Update Media_Salas.cpp

--- Media_Salas.cpp
+++ Media_Salas.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 //Programa Principal
 int main() {
-    //Variaveis
+    //Variaveis da media e do array
     float media;
     int sala[6];
     //Array das salas
@@ -13,14 +13,15 @@ int main() {
     sala[3] = 22;
     sala[4] = 36;
     sala[5] = 30;
-    //Condição para mostrar as salas para os usuarios
+    //Condição para mostrar a quantidade de alunos em cada sala para os usuarios
     for(int i = 0; i < 6; i++) {
         cout << "A sala " << i + 1 << " tem " << sala[i] << " alunos" << endl;
     }
     //Calculo da media e exibição da media para o usuario
     media = (sala[0] + sala[1] + sala[2] + sala[3] + sala[4] + sala[5]) / 6;
     cout << "A media é " << media << endl;
-    //Condição para ver se a sala esta acima ou abaixo da media
+    
+    //Condição para ver se cada sala esta acima ou abaixo da media
     if (sala[0] > media){
         cout << "A sala " << 1 << " está acima da media" << endl;
     }
